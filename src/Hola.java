1ad1b9f192e24e22f9598f8695479ad5004ce9dd
SMFN1209 <2203024160@cua.uam.mx>
2024-12-11 09:28:45 -0600

    Agregando operador ternario

---
+++ src/Hola.java
@@ -0,0 +1,12 @@
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo en java");
+        
+        int num1 = 10;
+        int num2 = 20;
+        String resultado = "";
+
+        resultado = num1 > num2 ? "El " + num1+ " es mayor que " + num2 : "El " + num2+ " es mayor que " + num1 ;
+
+    }
+}
